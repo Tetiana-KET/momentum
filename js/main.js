c84cd79efa47402e21650b5c40b1daa4951a854b
Tetiana-KET <belangelphone@gmail.com>
2022-08-14 15:07:42 +0300

    refactor: modified main.js, change setInterval to setTimeout

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@ const TIME = document.querySelector('.time');
 function showTime() {
     const DATE = new Date();// позволяет  получить текущие дату и время
     const currentTime = DATE.toLocaleTimeString();//из строки с датой и временем получить только время
-    TIME.innerHTML = currentTime; //отобразить внутри элемента dhtvz
+    TIME.innerHTML = currentTime; //отобразить внутри элемента 
+    setTimeout(showTime, 0);//setTimeout(func, 0) или просто setTimeout(func).
+    //Это планирует вызов func настолько быстро, насколько это возможно. 
+    //Но планировщик будет вызывать функцию только после завершения выполнения текущего кода.
 }
 
-setInterval(showTime, 1000);//Принимает функцию и второй параметр - интервал, через который запускает функцию
+showTime();
 
  
 
