9f67d0562497c1c3c5b2b49bb32d0a674fd67039
Tetiana-KET <belangelphone@gmail.com>
2022-08-14 20:04:09 +0300

    add local storage

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 const TIME = document.querySelector('.time');
 const DATE = document.querySelector('.date');
 const GREETING = document.querySelector('.greeting');
+const name = document.querySelector('.name');
 
 function getWeekDay() {
     const date = new Date();
@@ -24,11 +25,11 @@ function showGreeting() {
     const date = new Date();
     const hours = date.getHours();//Текущее время в часах возвращает метод getHours()
     const timesOfDayArray = ['morning', 'afternoon', 'evening', 'night'];
-
+    console.log(Math.trunc(hours));
     
-    let timeOfDay  = (Math.trunc(hours) > 18) ? timesOfDayArray[2] :
-        (Math.trunc(hours) > 12) ? timesOfDayArray[1] :
-        (Math.trunc(hours) > 6) ? timesOfDayArray[0] :
+    let timeOfDay  = (Math.trunc(hours) > 17) ? timesOfDayArray[2] :
+        (Math.trunc(hours) > 11) ? timesOfDayArray[1] :
+        (Math.trunc(hours) > 5) ? timesOfDayArray[0] :
         timesOfDayArray[3];
 
 
@@ -42,14 +43,28 @@ function showTime() {
     showDate();
     getWeekDay();
     showGreeting();
-    setTimeout(showTime, 0);//setTimeout(func, 0) или просто setTimeout(func).
+    setTimeout(showTime, 1000);//setTimeout(func, 0) или просто setTimeout(func).
     //Это планирует вызов func настолько быстро, насколько это возможно. 
     //Но планировщик будет вызывать функцию только после завершения выполнения текущего кода.
 }
 
 showTime();
 
+//Для сохранения имени пользователя используем хранилище браузера - local storage.
 
+function setLocalStorage() {
+  localStorage.setItem('name', name.value);
+};
+
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+};
+
+window.addEventListener('load', getLocalStorage);
 
 
 
